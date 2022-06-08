770f8905486e8cb427928b677f899fea76c46ed2
Matthew Christopher Albert <matthewmcavaio@gmail.com>
2022-06-08 17:17:19 +0700

    feat: add fonts

---
+++ Foneta/Themes/Fonts/AppFont.swift
@@ -0,0 +1,15 @@
+//
+//  AppFont.swift
+//  Foneta
+//
+//  Created by Matthew Christopher Albert on 08/06/22.
+//
+
+import SwiftUI
+
+enum AppFont: String, CaseIterable {
+    case jua = "Jua"
+    case interRegular = "Inter-Regular"
+    case interLight = "Inter-Light"
+    case interBold = "Inter-Bold"
+}
