f936b04eff1e242b392a110d74ca432f03ba11e0
Matthew Christopher Albert <matthewmcavaio@gmail.com>
2022-06-08 15:55:32 +0700

    feat: restructure

---
+++ Foneta/UI/Scenes/Home/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  Foneta
+//
+//  Created by Matthew Christopher Albert on 08/06/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    let model: HomeViewModel = HomeViewModel()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Foneta/UI/Scenes/Home/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewModel.swift
+//  Foneta
+//
+//  Created by Matthew Christopher Albert on 08/06/22.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    init() {
+        
+    }
+}
