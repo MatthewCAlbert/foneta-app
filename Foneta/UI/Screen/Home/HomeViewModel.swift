fc480fe44e1e6b4e1bada0bd02a8b4a52c65a77e
Matthew Christopher Albert <matthewmcavaio@gmail.com>
2022-07-04 10:23:38 +0700

    build: update settings for testflight

--- Foneta/UI/Screen/Home/HomeViewModel.swift
+++ Foneta/UI/Screen/Home/HomeViewModel.swift
@@ -25,7 +25,7 @@ class HomeViewModel: ObservableObject {
         } else {
             // MARK: Modify this to override first screen
             lastLevelId = mainStoryLane[0].id
-//            lastLevelId = "story-map-1"
+//            lastLevelId = "4"
         }
         if  UserData.shared.name != nil && UserData.shared.name != "" {
             skipNameAsk = true
